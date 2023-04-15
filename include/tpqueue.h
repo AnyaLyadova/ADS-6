d889faaf5ef4b43606c260ee491453de38373750
AnyaLyadova <124922006+AnyaLyadova@users.noreply.github.com>
2023-04-15 11:30:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,84 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+  T arr[size];
+  int first, last, count/*, prioriti*/;
+  void Transportation(int index, const SYM& value) {
+  int temp = first;
+  for (int i = last; i >= index; --i) {
+    arr[i + 1] = arr[i];
+  }
+  first = temp;
+  int in = index % size;
+  arr[index] = value;
+  last = (last + 1) % size;
+	}
+
+  int SearchInd(int prior) {
+    if (arr[first].GetPrior() < prior) {
+      return first;
+    } else if (arr[last].GetPrior() >= prior) {
+        return (last + 1) % size;
+      } else
+        {
+          int pr=last;
+			    for (int i = last; i >first; --i) {
+				    if (arr[i].GetPrior() < prior) {
+					  pr = i;
+					  continue;
+            } else
+					    break;
+			  }
+			return pr;
+	    }
+  }
+
+
+public:
+  TPQueue() : first(0), last(0), count(0) {};
+  bool isEmpty()const {
+    return count == 0;
+  }
+  bool isFull() const {
+    return count == size;
+  }
+  void  push(const SYM& value) {
+    if (isFull())
+      throw std::string("Full");
+    else if (isEmpty()) {
+      arr[first] = value;
+      ++count;
+		} else {
+			  int index = SearchInd(value.GetPrior());
+			  Transportation(index, value);
+		  	++count;
+		}
+	}
+  T& pop() {
+    T temp = arr[first];
+    first = (first + 1)%size;
+    --count;
+    return temp;
+  }
+  int First()const {
+    return first;
+  }
+  int Last()const {
+    return last;
+  }
 };
 
 struct SYM {
   char ch;
   int prior;
+  public:
+  char GetChar()const {
+    return ch;
+  }
+  int GetPrior() const {
+    return prior;
 };
+
 
 #endif  // INCLUDE_TPQUEUE_H_
